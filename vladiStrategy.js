87204b6ff83b7be7295357b72129624f3fbef109
Dian Dimitrov <diass1994@gmail.com>
2023-05-07 22:53:05 +0300

    Add files via upload

---
+++ vladiStrategy.js
@@ -0,0 +1,35 @@
+function findNumber(prisonerId, boxes) {
+  let numTries = 0;
+  let currentBox = Math.floor(Math.random() * boxes.length);
+  while (numTries < 50) {
+    numTries++;
+    if (currentBox === prisonerId - 1) {
+      return true;
+    }
+    currentBox = boxes[currentBox];
+  }
+  return false;
+}
+
+function runSimulation() {
+  let boxes = Array.from({ length: 100 }, (_, i) => i + 1);
+  boxes = boxes.sort(() => Math.random() - 0.5);
+  let numSuccesses = 0;
+  for (let i = 1; i <= 100; i++) {
+    if (findNumber(i, boxes)) {
+      numSuccesses++;
+    }
+  }
+  return numSuccesses === 100;
+}
+
+const numRuns = 1000;
+let numSuccesses = 0;
+for (let i = 0; i < numRuns; i++) {
+  if (runSimulation()) {
+    numSuccesses++;
+  }
+}
+const successRate = numSuccesses / numRuns;
+
+console.log(`Success rate over ${numRuns} runs: ${successRate}`);
